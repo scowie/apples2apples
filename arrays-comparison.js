0660235192bae3bc0ce06f353dfec9168a4d10a6
Stephen Cowie <cowiestephenjames@gmail.com>
2017-07-15 11:41:00 -0400

    initial mock data arrays

---
+++ arrays-comparison.js
@@ -0,0 +1,82 @@
+const people = [
+  {	
+		name: 'Amy',
+		age: 51,
+		car: 'BMW'
+	},
+	{
+		name: 'Brian',
+		age: 50,
+		car: 'Subaru'
+	},
+	{
+		name: 'Charles',
+		car: 'BMW'
+	},
+	{
+		name: 'Dennis',
+		age: 70,
+		car: 'Kia'
+	},
+	{
+		name: 'Eddie',
+		car: 'Toyota'
+	},
+	{
+		name: 'Kate',
+		car: 'BMW'
+	},
+	{
+		name: 'Mark',
+		car: 'Subaru'
+	},
+	{
+		name: 'Steve',
+		age: 40,
+		car: 'BMW'
+	}
+]
+
+const people2 = [
+
+  {
+		name: 'Alison'
+	},
+	{
+		name: 'Charles',
+		car: 'BMW'
+	},
+	{
+		name: 'Elizabeth',
+		age: 39,
+		car: 'Mazda'
+	},
+	{
+		name: 'Jenny',
+		car: 'Toyota'
+	},
+	{
+		name: 'Kate'
+	},
+	{
+		name: 'Mark'
+	},
+	{
+		name: 'Laura',
+		car:  'Nissan'
+	},
+	{
+		name: 'Marianne',
+		age: 51,
+		car: 'Mini'
+	},
+	{
+		name: 'Steve',
+		age: 40,
+		car: 'BMW'
+	},
+	{
+		name: 'Susan',
+		car: 'BMW'
+	}
+]
